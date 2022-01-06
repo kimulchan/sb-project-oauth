75d31bdf7d1d6f9a3cf0c558f1e0a0a430533459
kimuichan <leerabi577@gmail.com>
2022-01-06 10:40:31 +0900

    ⚡️ :: (#29) theme provider 및 global style 적용

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import GlobalStyle from "../styles/globalStyle";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@emotion/react";
+import theme from "../styles/theme";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
